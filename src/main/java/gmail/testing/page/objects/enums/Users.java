5598081ba681ed8e89aedabef873c1b0c03188c9
eugenekr <ykrasot11@students.lcc.lt>
2018-04-30 17:11:29 +0300

    changes in user info added

--- src/main/java/gmail/testing/page/objects/enums/Users.java
+++ src/main/java/gmail/testing/page/objects/enums/Users.java
@@ -4,8 +4,8 @@ package gmail.testing.page.objects.enums;
  * Created by HP on 2/1/2018.
  */
 public enum Users {
-    MAIN_USER("eugene.krasotski@gmail.com", "11235813f"),
-    ALTERNATIVE_USER("ykrasot11@students.lcc.lt", "5213420yauheni");
+    MAIN_USER("eugeneTestUserNumberOne@gmail.com", "11235813f"),
+    ALTERNATIVE_USER("eugeneTestUserNumberTwo@gmail.com", "11235813f");
 
     private String emailAddress;
     private String password;
