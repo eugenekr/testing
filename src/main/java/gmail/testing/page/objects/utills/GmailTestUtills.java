24ae288d057440a8b98d7e765a2ee8c41afc8115
eugenekr <ykrasot11@students.lcc.lt>
2018-04-30 18:14:08 +0300

    changes added due to switch to selenium 3

--- src/main/java/gmail/testing/page/objects/utills/GmailTestUtills.java
+++ src/main/java/gmail/testing/page/objects/utills/GmailTestUtills.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 
 
+import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -17,11 +18,13 @@ import java.util.concurrent.TimeUnit;
 public class GmailTestUtills {
 
     public static void waitForElementToBeVisible(WebDriver webdriver, WebElement element) {
+
         Wait<WebDriver> wait = new FluentWait<WebDriver>(webdriver)
-                .withTimeout(60, TimeUnit.SECONDS)
-                .pollingEvery(1, TimeUnit.SECONDS)
+                .withTimeout(Duration.ofSeconds(20, 10))
+                .pollingEvery(Duration.ofSeconds(1,10))
                 .ignoring(ElementNotVisibleException.class)
                 .ignoring(StaleElementReferenceException.class)
+                .ignoring(WebDriverException.class)
                 .ignoring(NoSuchElementException.class);
 
         wait.until(ExpectedConditions.visibilityOf(element));
@@ -29,8 +32,8 @@ public class GmailTestUtills {
 
     public static void waitForElementToBeNotVisible(WebDriver webdriver, List<WebElement> elementList) {
         Wait<WebDriver> wait = new FluentWait<WebDriver>(webdriver)
-                .withTimeout(30, TimeUnit.SECONDS)
-                .pollingEvery(1, TimeUnit.SECONDS);
+                .withTimeout(Duration.ofSeconds(20, 10))
+                .pollingEvery(Duration.ofSeconds(1, 10));
 
         wait.until(ExpectedConditions.invisibilityOfAllElements(elementList));
     }
